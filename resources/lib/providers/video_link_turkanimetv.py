c1f43e624cf12fd420f06298d0342e079c268a49
Hagai Cohen <hagai.co@gmail.com>
2015-11-28 16:44:14 +0200

    Added url translation for turkanimetv
    Signed-off-by: Hagai Cohen <hagai.co@gmail.com>

--- resources/lib/providers/video_link_turkanimetv.py
+++ resources/lib/providers/video_link_turkanimetv.py
@@ -1,9 +1,44 @@
 import re
 from unidecode import unidecode
-			
+
 domain="http://www.turkanime.tv/"
 encoding="utf-8"
 
+def fix_url(url, referer):
+	# fix links that are missing http://
+	if url.startswith("//"):
+		url = "http:%s" % url
+
+	# MAIL abstraction
+	r=re.match("https\://www\.schneizel\.net/video/mail\.php\?vid\=(.+)", url)
+	if r:
+		return [r.group(1).decode("base64")]
+
+	# GPLUS abstraction
+	r=re.match("(http\://www\.schneizel\.net/video/plus\.php\?vid\=.+)", url)
+	if r:
+		mpage=ump.get_page(r.group(1), encoding, referer=referer)
+		r=re.match("sources:\s(\[.+?\]),", mpage, re.DOTALL)
+		if not r:
+			return []
+		print r.group(1)
+		# TODO: Find a page with working sources and finish this.
+		return []
+
+	# VK abstraction
+	r=re.match("http\://www\.schneizel\.net/video/vk3\.php\?vid\=(.+)", url)
+	if r:
+            oid,video_id,embed_hash=r.group(1).split("_")
+            return ["http://vk.com/video_ext.php?oid="+oid+"&id="+video_id+"&hash="+embed_hash]
+
+	# TURKANIME abstraction
+	r=re.match("(http\://www\.schneizel\.net/video/index\.php\?vid\=.+)", url)
+	if r:
+		print "R!!!!!"
+		print r.group(1)
+		return [url]
+	print url
+	return [url]
 
 def return_links(name,url,fs):
 	mpage=ump.get_page(url,encoding)
@@ -17,12 +52,14 @@ def return_links(name,url,fs):
 			return None
 		video_url = links[0]
 
-	parts=[{"url": video_url}]
-	prefix=""
-	if not fs == "Varsayilan":
-		prefix="[FS:%s]"%fs
-	name="%s[HS:TR]%s" % (prefix,name)
-	ump.add_mirror(parts,name)
+	video_urls = fix_url(video_url, url)
+	for video_url in video_urls:
+		parts=[{"url": video_url}]
+		prefix=""
+		if not fs == "Varsayilan":
+			prefix="[FS:%s]"%fs
+		name="%s[HS:TR]%s" % (prefix,name)
+		ump.add_mirror(parts,name)
 
 def scrape_moviepage(url,fansub,name):
 	pg=ump.get_page(domain+url,encoding)
@@ -31,7 +68,7 @@ def scrape_moviepage(url,fansub,name):
 		url = domain+video[0]
 		return_links(name, url, fansub)
 		continue
-		
+
 def run(ump):
 	globals()['ump'] = ump
 	i=ump.info
